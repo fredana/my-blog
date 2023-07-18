0fbe6bfa06cc2281cae5364b87ec10d25d8d11fd
Farida NDiaye <faridandiaye60@gmail.com>
2023-07-18 00:32:02 -0400

    MESSAGE CONFIRMATION UPDATED

--- main.js
+++ main.js
@@ -28,91 +28,10 @@ function onClick(element) {
   }
 
 
-  document.addEventListener('DOMContentLoaded', () => {
-    let form = document.querySelector('form')
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        confirmBookingText(`Name: ${e.target.fname.value}`);
-        confirmBookingText(`Email: ${e.target.email.value}`);
-        confirmBookingText(`Message: ${e.target.message.value}`);
-        // confirmBookingText(`Guest(s) : ${e.target.adult.value} adult(s)`)
-        // confirmBookingText(`And : ${e.target.child.value} kid(s)`)
-        // confirmBookingText(`Room Size : ${e.target.typeRoom.value}`)
-        // confirmBookingText(`Check-in Date : ${e.target.comeIn.value}`)
-        // confirmBookingText(`Check-out Date : ${e.target.goOut.value}`)
-        // confirmationHeading(e.target.value)
-        bookingConfirmationBtn(e.target.value)   
-        // form.reset();
-    })
-})
 
-//*****************  BOOKING CONFIRMATION SECTION  *****************
-function confirmationHeading(){
-    let h3 = document.createElement('h2')
-    h3.textContent = 'EMAIL CONFIRMATION'
-    h3.style.textDecoration = 'underline'
-    h3.style.padding = '10px'
-    h3.style.marginTop = '5px'
-    h3.style.color = 'blue'
-    h3.style.backgroundColor = 'yellow'
-    h3.style.width = '340px'
-    document.querySelector('#h3').appendChild(h3)
-}
-confirmationHeading()
+//*****************  EMAIL CONFIRMATION SECTION  *****************
 
-//-------  CONFIRMATION TEXT  ---------
-function confirmBookingText(text){    
-    let p = document.createElement('p')
-    p.textContent = text 
-    p.style.fontWeight='bold'  
-    p.style.fontSize='25px'
-    p.style.backgroundColor = 'lightgray'
-    document.querySelector('#confirmation').appendChild(p)
-}
-
-//-----  CONFIRM & ResetAll SECTION  ----
-function bookingConfirmationBtn(){
-    let btn = document.createElement('button')
-    btn.textContent = 'Confirm'
-    btn.style.color ='green'
-    btn.style.fontWeight ='bold'
-    btn.style.fontSize ='25px'
-    btn.style.borderRadius ='10px'
-    btn.addEventListener('mouseover', bgColorSubmit)
-    btn.addEventListener('mouseleave', alertNotSubmit)
-    btn.addEventListener('click', confirmMessage)
-
-    //.... RESET ALL BUTTON ....
-    let resetAllBtn = document.createElement('button')
-    resetAllBtn.textContent = 'Reset All'
-    resetAllBtn.style.fontSize = '20px'
-    resetAllBtn.style.borderRadius ='7px'
-    resetAllBtn.addEventListener('mousemove', bgColorReset)
-    resetAllBtn.addEventListener('mouseleave', alertNoAction)
-    resetAllBtn.addEventListener('click', handleDelete2)
-    
-    document.querySelector('#confirmation').appendChild(btn)
-    document.querySelector('#confirmation').appendChild(resetAllBtn)
-}
-
-function confirmMessage(){
-    const list2 = document.getElementById("confirmation");
-    list2.innerHTML = 'THANKS! WE WILL RESPOND WITHIN 2 BUSINESS DAYS.'
-    list2.style.color = 'green'
-    list2.style.backgroundColor = 'cyan'
-    list2.style.fontSize = '30px'
-    list2.style.fontFamily = 'cursive'
-    list2.style.width = '300px'
-    
-}
-
-function handleDelete2(){
-    const list = document.getElementById("confirmation");
-
-    while (list.hasChildNodes()) {
-        list.removeChild(list.firstChild);
-    }
-}
+//-----  SEND & RESET SECTION  ----
 
 function bgColorSubmit(bsbtn){
     bsbtn.target.style.backgroundColor = 'lightgreen'
@@ -130,10 +49,6 @@ function alertNotSubmit(bsbtn){
 }
 
 
-function resetAll(){
-
-}
-
 function submitBtn(){
     let btn = document.querySelector('#submit')
     btn.addEventListener('mouseover', bgColorSubmit)
@@ -141,7 +56,7 @@ function submitBtn(){
 }
 submitBtn()
 
-//******************   RESET BUTTON IN BOOKING A ROOM SECTION  *******************
+//******************   RESET BUTTON SECTION  *******************
 function bgColorReset(brbtn){
     brbtn.target.style.backgroundColor = 'orange'
     brbtn.target.style.color ='red'
@@ -168,3 +83,19 @@ function resetBtn(){
     btn.addEventListener('click', handleDelete)
 }
 resetBtn()
+
+function confirmMessage2(){
+    const list2 = document.getElementById("fill");
+    list2.innerHTML = 'THANKS! WE WILL RESPOND WITHIN 2 BUSINESS DAYS.'
+    list2.style.color = 'green'
+    list2.style.backgroundColor = 'cyan'
+    list2.style.fontSize = '30px'
+    list2.style.fontFamily = 'cursive'
+    list2.style.width = '300px'
+}
+
+function jeconfirm(){
+    let confirmer = document.getElementById('submit');
+    confirmer.addEventListener('click', confirmMessage2);
+}
+jeconfirm();
